ef2fc5148cb2bd2973c2e9a321c501ad5bc33b44
Arham-Jawed <arham0jawed@gmail.com>
2024-11-29 09:38:20 +0530

    Change In Backend

--- server/src/index.js
+++ server/src/index.js
@@ -31,10 +31,10 @@ app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(_dirname, "../../client/dist")));
+  app.use(express.static(path.join(_dirname, "../client/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(_dirname, "../../client", "dist", "index.html"));
+    res.sendFile(path.join(_dirname, "../client", "dist", "index.html"));
   });
 }
 
